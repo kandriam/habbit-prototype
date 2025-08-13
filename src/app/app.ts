a4797a72414ce90b491c1ded646590567cbe801b
Kaliana Andriamananjara <kaliana.rmac@gmail.com>
2025-08-13 13:05:17 -0500

    Date exists
    Habits seem to be saving, issue was with timesperinstance and type

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,8 +7,9 @@ import {RouterModule} from '@angular/router';
   imports: [Home, RouterModule],
   template: `
     <main>
-      <header class="brand-name">
+      <header>
         <a [routerLink]="['/']"><h1>Habbit</h1></a>
+        <div id="date"></div>
       </header>
       <section class="content">
         <router-outlet></router-outlet>
@@ -20,3 +21,19 @@ import {RouterModule} from '@angular/router';
 export class App {
   title = 'habbit';
 }
+
+window.onload = () => {
+    displayDate();
+}
+function displayDate() {
+    let months = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let date =  new Date();
+    let y = date.getFullYear();
+    let m = date.getMonth() + 1;
+    let d = date.getDate();
+    var datedisplay = document.getElementById("date");
+    datedisplay!.innerHTML = (`
+      <h3>${m} / ${d} / ${y}</h3>
+      <h4>${months[m-1]} ${d}, ${y}</h4>
+    `)
+  };
